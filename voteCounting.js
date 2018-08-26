dae9e0c179cc99c5916c0cd4a0d44adb59485d98
Ben Liu <ben196888@gmail.com>
2018-08-26 12:35:18 +1000

    ✨ voteCounting
    Add voting logic in vote counting

---
+++ voteCounting.js
@@ -0,0 +1,53 @@
+module.exports = {
+  voteCounting,
+};
+
+function voteCounting(ballotBoard) {
+  let candidateId;
+  do {
+    console.log(ballotBoard.toString());
+    candidateId = nextRound(ballotBoard);
+  } while (candidateId === -1);
+  return candidateId;
+}
+
+// TODO: refactor to be generator function
+function nextRound(ballotBoard) {
+  const candidate = ballotBoard.candidate;
+  const ballotColumns = ballotBoard.ballotColumns;
+  const notEliminatedCols = ballotColumns.filter((col, i) => candidate.isNotEliminatedId(i));
+  const nonExhaustedBallots = notEliminatedCols.reduce((sum, col) => { sum += col.length; return sum; }, 0);
+  const maxCandidate = ballotColumns.reduce((res, col, i) => {
+    if (candidate.isNotEliminatedId(i) && col.length > res.max) {
+      res.max = col.length;
+      res.id = i;
+    }
+    return res;
+  }, { max: 0, id: -1 });
+  const minCandidate = ballotColumns.reduce((res, col, i) => {
+    if (candidate.isNotEliminatedId(i) && col.length < res.min) {
+      res.min = col.length;
+      res.id = i;
+    }
+    return res;
+  }, { min: nonExhaustedBallots, id: -1 });
+  const winThreshold = Math.ceil(nonExhaustedBallots / 2);
+
+  // return candidate id
+  if (maxCandidate.max >= winThreshold) {
+    return maxCandidate.id;
+  }
+
+  // Eliminate minimum column
+  candidate.eliminateCandidateById(minCandidate.id);
+
+  // Relocate the ballot in eliminated column
+  const ballotsShouldBeRelocated = ballotColumns[minCandidate.id];
+  for (const ballot of ballotsShouldBeRelocated) {
+    console.log(`Relocate ballot ${ballot.toString()}`);
+    ballotBoard.allocateBallot(ballot);
+  }
+
+  // return means not found result yet
+  return -1;
+}
